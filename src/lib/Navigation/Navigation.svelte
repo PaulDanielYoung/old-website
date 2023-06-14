d3198d367f567ff9de0812273cd1944adde75f7f
Paul Young <pauldanielyoung@outlook.com>
2023-06-13 22:48:45 -0400

    Added projects tab

--- src/lib/Navigation/Navigation.svelte
+++ src/lib/Navigation/Navigation.svelte
@@ -20,6 +20,7 @@
 <nav class="px-4 py-0 md:py-2 md:p-0">
 	<ul class="md:flex md:space-x-2 md:justify-center md:items-center space-y-4 md:space-y-0">	
 		<li><a href="/" class={routeId === '/' ? "btn btn-sm bg-primary-300-600-token unstyled font-sans" : "btn btn-sm unstyled font-sans"} on:click={drawerClose}>Home</a></li>
+		<li><a href="/projects" class={routeId.startsWith('/projects') ? "btn btn-sm bg-primary-300-600-token unstyled font-sans" : "btn btn-sm unstyled font-sans"} on:click={drawerClose}>Projects</a></li>
 		<li><a href="/blog" class={routeId.startsWith('/blog') ? "btn btn-sm bg-primary-300-600-token unstyled font-sans" : "btn btn-sm unstyled font-sans"} on:click={drawerClose}>Blog</a></li>
 		<li><a href="/about" class={routeId === '/about' ? "btn btn-sm bg-primary-300-600-token unstyled font-sans" : "btn btn-sm unstyled font-sans"} on:click={drawerClose}>About</a></li>
 	</ul>
