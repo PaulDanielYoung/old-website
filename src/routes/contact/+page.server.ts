aef583900d402d0b204170918d240a7fa4b277b0
Paul Young <pauldanielyoung@outlook.com>
2023-06-15 03:33:12 -0400

    Form submission is working!

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,6 +1,7 @@
 import { superValidate } from 'sveltekit-superforms/server';
 import { fail } from '@sveltejs/kit';
 import { z } from 'zod';
+import nodemailer from 'nodemailer'
 
 import type { Actions, PageServerLoad } from './$types';
 
@@ -30,6 +31,32 @@ export const actions: Actions = {
 		}
 
 		// TODO: Do something with the validated data
+
+		// Set up Nodemailer with SMTP2GO
+		const transporter = nodemailer.createTransport({
+			host: "mail.smtp2go.com",
+			port: 2525, // 8025, 587 and 25 can also be used.
+			auth: {
+				user: process.env.SMTP2GO_USERNAME,
+				pass: process.env.SMTP2GO_PASSWORD,
+			}
+		});
+
+		// Set up the email data
+		const mailOptions = {
+			from: '"Website Form" <admin@paulyoung.fun>', // sender address
+			to: 'pauldanielyoung@outlook.com', // list of receivers
+			subject: 'Test Form Submission', // Subject line
+			text: `Name: ${form.data.name}\nEmail: ${form.data.email}`, // plain text body
+		};
+
+		// Send the email
+		transporter.sendMail(mailOptions, (error, info) => {
+			if (error) {
+				return console.log(error);
+			}
+			console.log('Message sent: %s', info.messageId);
+		});
 
 		// Yep, return { form } here too
 		return { form };
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { mdsvex } from 'mdsvex';
